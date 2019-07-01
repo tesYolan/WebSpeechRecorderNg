ec33d36804e8945ba589804ab6bc3a64e8f1e847
klausj <klausj@phonetik.uni-muenchen.de>
2019-07-01 12:43:00 +0200

    Upgrade Angular 7 to 8. Stage 4

--- projects/speechrecorderng/src/lib/speechrecorder/session/sessionmanager.ts
+++ projects/speechrecorderng/src/lib/speechrecorder/session/sessionmanager.ts
@@ -114,8 +114,8 @@ export class SessionManager implements AfterViewInit,OnDestroy, AudioCaptureList
   ac: AudioCapture;
   private _channelCount = 2; //TODO define constant for default format
   private _selectedDeviceId:string|null=null;
-  @ViewChild(Prompting) prompting: Prompting;
-  @ViewChild(LevelBarDisplay) liveLevelDisplay: LevelBarDisplay;
+  @ViewChild(Prompting, { static: true }) prompting: Prompting;
+  @ViewChild(LevelBarDisplay, { static: true }) liveLevelDisplay: LevelBarDisplay;
 
 
   startStopSignalState: StartStopSignalState;
